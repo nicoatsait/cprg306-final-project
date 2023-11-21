1e3d83a7e5e2d4ff28ce4416da1f12038b51abcc
fudge54 <danman250000@gmail.com>
2023-11-21 14:31:05 -0700

    Update Random-joke.js

--- app/Random-joke.js
+++ app/Random-joke.js
@@ -1,7 +1,40 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import Heading from "./heading.js";
-import Joke from "./joke";
+import Joke from "./Joke";
 
-async function 
+export default function RandomJoke({ onAddFavorite }) {
+    const [joke, setJoke] = useState(null);
+
+    async function loadRandomJoke() {
+        try {
+            const categoriesToExclude = ["explicit", "political", "religion"];
+            let response, newJoke;
+      
+            do {
+              response = await fetch("https://api.chucknorris.io/jokes/random");
+              newJoke = await response.json();
+            } while (categoriesToExclude.includes(newJoke.category));
+      
+            setJoke(newJoke);
+          } catch (error) {
+            console.error("Error fetching Chuck Norris joke:", error);
+          }
+    }
+
+    useEffect(() => {
+        loadRandomJoke();
+    }, []);
+
+    return (
+        <div>
+          {joke ? (
+            <Joke joke={joke} onAddFavorite={onAddFavorite} />
+          ) : (
+            "Loading..."
+          )}
+          <button onClick={loadRandomJoke}>Get Another Joke</button>
+        </div>
+      );
+    };
+    
