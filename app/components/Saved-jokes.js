9e82ba76c3c7abf0437ed238487490796c6706f0
nicoatsait <119918552+nicoatsait@users.noreply.github.com>
2023-11-21 14:46:55 -0700

    no HEADING

--- app/components/Saved-jokes.js
+++ app/components/Saved-jokes.js
@@ -3,7 +3,7 @@ import Joke from './Joke';
 export default function SavedJokes ({ jokes, removeJoke }) {
   return (
     <section>
-        <Heading title="My Saved Jokes" />
+        <h2>My Saved Jokes</h2>
         {jokes.length > 0 
             ? jokes.map(joke => (
                 <Joke joke={joke} key={joke.id} removeJoke={removeJoke} />
