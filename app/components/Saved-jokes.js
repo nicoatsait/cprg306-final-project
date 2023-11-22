3490d2143846d46e82e099adcb44e436870f6d3b
nicoatsait <119918552+nicoatsait@users.noreply.github.com>
2023-11-22 04:16:36 -0700

    make the code work

--- app/components/Saved-jokes.js
+++ app/components/Saved-jokes.js
@@ -2,14 +2,17 @@ import Joke from './Joke';
 
 export default function SavedJokes ({ jokes, removeJoke }) {
   return (
-    <section>
-        <Heading title="My Saved Jokes" />
-        {jokes.length > 0 
+    <section className='text-slate-950'>
+        <h2 className=''>My Saved Jokes</h2>
+        <div className='text-red-700'>
+            {jokes.length > 0 
             ? jokes.map(joke => (
                 <Joke joke={joke} key={joke.id} removeJoke={removeJoke} />
                 ))
             : "You don't have any saved jokes yet."
         }
+        </div>
+        
     </section>
   );
 }
--- app/page.js
+++ app/page.js
@@ -1,9 +1,26 @@
+"use client";
+
 import Image from 'next/image';
 import Link from 'next/link';
+import { useState, useEffect } from 'react';
 import StudentsInfo from './components/studentsInfo';
+import RandomJoke from './components/Random-joke';
+import SavedJokes from './components/Saved-jokes';
 
 
 export default function Home() {
+  const [savedJokes, setSavedJokes] = useState([]);
+
+  function handleAddSaved(joke){
+    if (joke && !savedJokes.some((q) => q.id === joke.id)) {
+      setSavedJokes((prevJokes) => [...savedJokes, joke]);
+    }
+  }
+
+  function handleRemoveSaved(joke){
+    setSavedJokes((prevJokes) => prevJokes.filter((q) => q.id !== joke.id));
+  }
+
   return (
     <main className='flex flex-col items-center justify-center min-h-screen bg-gray-50'>
       <div className="text-4xl font-bold text-blue-800 my-8">
@@ -12,11 +29,14 @@ export default function Home() {
 
         <StudentsInfo />
 
-      <div className="grid grid-cols-3 gap-4 m-4">
-          <Link href="Joke" className="flex-1 text-center bg-blue-100 hover:bg-blue-300 transition duration-300 ease-in-out cursor-pointer p-6 rounded-lg">Generate Joke</Link>
-          <Link href="Random-joke" className="flex-1 text-center bg-blue-100 hover:bg-blue-300 transition duration-300 ease-in-out cursor-pointer p-6 rounded-lg">Random Joke</Link>
-          <Link href="Saved-jokes" className="flex-1 text-center bg-blue-100 hover:bg-blue-300 transition duration-300 ease-in-out cursor-pointer p-6 rounded-lg">Saved Jokes</Link>
-
+      <div className=" text-zinc-900  grid grid-cols-3 gap-4 m-4">
+         <RandomJoke onAddFavorite={handleAddSaved} />
+         <hr className="my-4" />
+         <SavedJokes
+            jokes={savedJokes}
+            removeJoke={handleRemoveSaved}
+         />
+         
       </div>
         
     </main>
